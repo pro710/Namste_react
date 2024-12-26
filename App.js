510a2e1024e82f04964b763035bc495379692643
Mayank <abc@abcs-MacBook-Air.local>
2024-12-26 20:41:22 +0530

    first

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+// const Header = () =>{
+//     return(
+//         <h1>hii</h1>
+//     );
+// }
+
+const Heading = () =>{
+    
+        return(
+            <div>
+            <h1>hii</h1>
+            <h2>hi</h2>
+            </div>
+        )
+    
+}
+
+// const root =ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Heading/>)
+ReactDOM.render(<Heading />, document.getElementById("root"));
